b16ccb6bc83be5fe2ac13fea0a0982cd789ce7d3
Abhi Sharma <161916643+Abhi-Sharma@users.noreply.github.com>
2025-09-29 18:09:29 +0530

    Time: 7 ms (32.3%), Space: 18.8 MB (26.52%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,35 +1,31 @@
 class Solution {
-    private:
-    bool dfs(int node, vector<int>& vis, vector<vector<int>>& adj,
-     vector<int>& order){
-        vis[node] = 1;
-        for(auto it : adj[node]){
-            if(vis[it] == 0){
-                if(dfs(it,vis,adj,order) == true) return true;
-            }
-            else if(vis[it] == 1) return true;
-        }
-        vis[node] = 2;
-        order.push_back(node);
-        return false;
-    }
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(numCourses);
-        for (auto &e : prerequisites)
-        {
-            int u = e[0], v = e[1];
-            adj[v].push_back(u);
+        vector<int> indegree(numCourses,0);
+      for (auto p : prerequisites) {
+            int a = p[0]; 
+            int b = p[1]; 
+            adj[b].push_back(a);
+            indegree[a]++;
         }
 
-        vector<int> vis(numCourses, 0), pathVis(numCourses, 0), order;
-        for(int i = 0 ; i < numCourses; i++){
-            if(vis[i] == 0){
-                if(dfs(i,vis,adj,order)) return {};
+        queue<int> q;
+        for(int i = 0; i < numCourses; i++){
+            if(indegree[i] == 0) q.push(i);
+        }
+
+        vector<int> courses;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            courses.push_back(node);
+            for(auto it : adj[node]){
+                indegree[it]--;
+                if(indegree[it] == 0) q.push(it);
             }
         }
-
-        reverse(order.begin(),order.end());
-        return order;
+      if(courses.size() == numCourses)  return courses;
+      return {};
     }
 };
