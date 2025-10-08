0943ae707cc96d86852aa94c5f7e47ee5f69f7a9
Abhi Sharma <161916643+Abhi-Sharma@users.noreply.github.com>
2025-10-08 21:11:07 +0530

    Time: 0 ms (100%), Space: 19.5 MB (75.93%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,41 +1,50 @@
-class Solution {
-    void bfs(int node, vector<vector<int>> adjls, vector<int>& vis){
-        vis[node] = 1;
-        queue<int> q;
-        q.push(node);
-        while(!q.empty()){
-            int n = q.front();
-            q.pop();
-            for(auto it : adjls[n]){
-                if(!vis[it]){
-                    vis[it] = 1;
-                    q.push(it);
-                }
-            }
+class DisjointSet{
+    public:
+    vector<int> parent,size;
+    DisjointSet(int n){
+        parent.resize(n+1);
+        size.resize(n+1);
+        for(int i = 0; i < n; i++){
+            parent[i] = i;
+            size[i] = 1;
         }
     }
+
+    int findUPar(int node){
+        if(node == parent[node]) return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+    void unionBySize(int u, int v){
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if(ulp_u == ulp_v) return;
+        if(size[ulp_u] < size[ulp_v]){
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else{
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int V = isConnected.size();
-        vector<vector<int>> adjls(V);
+        DisjointSet ds(V);
         for(int i = 0; i < V; i++){
             for(int j = 0; j < V; j++){
-                if(isConnected[i][j] == 1 && i != j){
-                    adjls[i].push_back(j);
-                    adjls[j].push_back(i);
+                if(isConnected[i][j] == 1){
+                    ds.unionBySize(i,j);
                 }
             }
         }
 
-        vector<int> vis(V,0);
-        int count = 0;
-        for(int i = 0; i < V; i++){
-            if(vis[i] == 0){
-                count++;
-                bfs(i,adjls,vis);
-            }
+        int cnt = 0;
+        for(int i = 0 ; i < V; i++){
+            if(ds.parent[i] == i) cnt++;
         }
-
-        return count;
+        return cnt;
     }
 };
