7e5ed6730cea6fea2419d485c79ee739f2cbe76a
Abhi Sharma <161916643+Abhi-Sharma@users.noreply.github.com>
2025-10-14 18:44:47 +0530

    Time: 19 ms (76.34%), Space: 17.5 MB (27.1%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -7,16 +7,17 @@ public:
         for(int i = 0; i < n; i++){
             if(i > 0 && nums[i] == nums[i-1]) continue;
             for(int j = i + 1; j < n; j++){
-                if(j > i + 1 && nums[j] == nums[j-1]) continue;
+                if( j > i + 1 && nums[j] == nums[j-1]) continue;
                 int k = j + 1;
                 int l = n - 1;
                 while(k < l){
-                    long long sum = nums[i];
-                    sum += nums[j];
-                    sum += nums[k];
-                    sum += nums[l];
-                    if(sum < target) k++;
-                    else if(sum > target) l--;
+                    long long sum = (long long) nums[i] + nums[j] + nums[k] + nums[l];
+                    if(sum < target){
+                        k++;
+                    }
+                    else if(sum > target){
+                        l--;
+                    }
                     else{
                         vector<int> temp = {nums[i],nums[j],nums[k],nums[l]};
                         ans.push_back(temp);
