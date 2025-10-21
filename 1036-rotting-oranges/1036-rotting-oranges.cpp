61b635cf0db333a0159e9c3d63c0ccfe2f0be6c4
Abhi Sharma <161916643+Abhi-Sharma@users.noreply.github.com>
2025-10-21 15:01:26 +0530

    Time: 0 ms (100%), Space: 17.2 MB (32.85%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,46 +1,44 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
+        queue<pair<pair<int, int>, int>> q;
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> vis(n, vector<int>(m, 0));;
-        queue<pair<pair<int,int>,int>> q;
-        int cntfresh = 0;
-        for(int i = 0; i < n ; i++){
+        vector<vector<int>> vis(n , vector<int>(m,0));
+        for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(grid[i][j] == 2){
-                    q.push({{i,j},0});
                     vis[i][j] = 2;
+                    q.push({{i,j},0});
                 }
-                else vis[i][j] = 0;
-                if(grid[i][j] == 1) cntfresh++; 
             }
         }
-
-        int t_max = 0;
-        int cnt = 0;
-        int delrow[] = {-1,0,+1,0};
-        int delcol[] = {0,+1,0,-1};
+        int count = 0;
         while(!q.empty()){
-            int r = q.front().first.first;
-            int c = q.front().first.second;
-            int t = q.front().second;
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int time = q.front().second;
             q.pop();
-            t_max = max(t_max,t);
+            count = max(time,count);
 
+            int delrow[] = {-1,0,1,0};
+            int delcol[] = {0,1,0,-1};
             for(int i = 0; i < 4; i++){
-                int nrow = delrow[i] + r;
-                int ncol = delcol[i] + c;
-                if(nrow >=0 && nrow < n && ncol >=0 && ncol < m && vis[nrow][ncol] != 2 && 
-                grid[nrow][ncol] == 1){
-                    q.push({{nrow,ncol},t + 1});
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i];
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && vis[nrow][ncol] != 2 && grid[nrow][ncol] == 1){
                     vis[nrow][ncol] = 2;
-                    cnt++;
+                    q.push({{nrow,ncol}, time + 1});
                 }
             }
-
         }
-        if(cnt != cntfresh) return -1;
-        return t_max;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(grid[i][j] == 1 && vis[i][j] != 2){
+                    return -1;
+                }
+            }
+        }
+        return count;
     }
 };
