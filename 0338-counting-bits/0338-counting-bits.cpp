693b33c3950aa2a3517c0a912ea929e0056bd1f0
Abhi Sharma <161916643+Abhi-Sharma@users.noreply.github.com>
2026-03-02 13:13:44 +0530

    Time: 0 ms (100%), Space: 11.5 MB (9.46%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans;
+
+        for(int i = 0; i <= n; i++){
+            int num = i;
+            int cnt = 0;
+            while(num >= 1){
+                cnt += (num & 1);
+                num >>= 1;
+            }
+            ans.push_back(cnt);
+        }
+        return ans;
+    }
+};
