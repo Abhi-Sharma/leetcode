d2ca463eeda7ab457b045020a81ed9e36cf0493e
Abhi Sharma <161916643+Abhi-Sharma@users.noreply.github.com>
2026-03-23 18:21:26 +0530

    Time: 0 ms (100%), Space: 9.6 MB (91.34%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int freq[26] = {0};
-        for(int i = 0; i < s.size(); i++){
-            freq[s[i] - 'a']++;
-        }
-        for(int i = 0; i < t.size(); i++){
-            freq[t[i] - 'a']--;
-        }
+        int m = s.size();
+        int n = t.size();
+        if(m != n) return false;
 
-        for(int i = 0; i < 26; i++){
-            if(freq[i] != 0) return false;
-        }
+        int freq[26] = {0};
+        for(int i = 0; i < m; i++) freq[s[i] - 'a']++;
+
+        for(int i = 0; i < n; i++) freq[t[i] - 'a']--;
+
+        for(int i = 0; i < n; i++) if(freq[t[i] - 'a'] != 0) return false;
+
         return true;
     }
 };
