596d4917d021bdf17f67d9c52123a9a0c44ebd7b
Abhi Sharma <161916643+Abhi-Sharma@users.noreply.github.com>
2026-05-17 10:31:50 +0530

    Time: 3 ms (45.57%), Space: 36.6 MB (60.85%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -6,14 +6,20 @@ public:
 
         queue<int> q;
         q.push(start);
+        vis[start] = 1;
         while(!q.empty()){
             int ind = q.front();
-            vis[ind] = 1;
             q.pop();
 
             if(arr[ind] == 0) return true;
-            if(ind + arr[ind] < n && !vis[ind + arr[ind]]) q.push(ind + arr[ind]);
-            if(ind - arr[ind] >= 0 && !vis[ind - arr[ind]]) q.push(ind - arr[ind]); 
+            if(ind + arr[ind] < n && !vis[ind + arr[ind]]){
+                vis[ind + arr[ind]] = 1;
+                q.push(ind + arr[ind]);
+            }
+            if(ind - arr[ind] >= 0 && !vis[ind - arr[ind]]){
+                vis[ind - arr[ind]] = 1;
+                q.push(ind - arr[ind]);
+            } 
         }
         return false;
     }
