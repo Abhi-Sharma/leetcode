7e2f01615cc0414a149a00e4c20d5f9ae02c44aa
Abhi Sharma <161916643+Abhi-Sharma@users.noreply.github.com>
2026-07-03 13:28:51 +0530

    Time: 6 ms (44.97%), Space: 23.8 MB (82.35%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -3,19 +3,19 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         vector<vector<int>> ans;
         sort(intervals.begin(),intervals.end());
-        int currentStart = intervals[0][0];
-        int lastTime = intervals[0][1];
+        int start = intervals[0][0];
+        int end = intervals[0][1];
         for(int i = 1; i < intervals.size(); i++){
-            if(intervals[i][0] <= lastTime){
-                lastTime = max(lastTime,intervals[i][1]);
+            if(end >= intervals[i][0]){
+                end = max(end,intervals[i][1]);
             }
             else{
-                ans.push_back({currentStart,lastTime});
-                currentStart = intervals[i][0];
-                lastTime = intervals[i][1];
+                ans.push_back({start,end});
+                start = intervals[i][0];
+                end = intervals[i][1];
             }
         }
-        ans.push_back({currentStart,lastTime});
+        ans.push_back({start,end});
         return ans;
     }
 };
